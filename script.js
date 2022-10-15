20bf9d330583f123c5bb8a026a40a3d1a1888b64
Satyam <satyam19arya@gmail.com>
2022-10-16 00:50:22 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+// navbar scroll section
+let nav = document.querySelector(".navigation-wrap");
+window.onscroll = function(){
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("scroll-on");
+    }else{
+        nav.classList.remove("scroll-on");
+    }
+}
+
+// nav hide
+let navBar = document.querySelectorAll('.nav-link');
+let navCollapse = document.querySelector('.navbar-collapse.collapse');
+navBar.forEach(function(a){
+    a.addEventListener("click", function(){
+        navCollapse.classList.remove("show");
+    })
+})
+
+// count section
+document.addEventListener("DOMContentLoaded", () =>{
+    function counter(id, start, end, duration){
+        let obj = document.getElementById(id),
+        current = start,
+        range = end - start,
+        increment = end > start ? 1 : -1,
+        step = Math.abs(Math.floor(duration / range)),
+        timer = setInterval(() =>{
+            current += increment;
+            obj.textContent = current;
+            if(current == end){
+                clearInterval(timer);
+            }
+        },step);
+    }
+    counter("count1", 0, 2000, 2000);
+    counter("count2", 0, 900, 2000);
+    counter("count3", 0, 2000, 2000);
+    counter("count4", 0, 1200, 2000);
+});
